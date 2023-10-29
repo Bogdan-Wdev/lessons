ec2e56f11af6a90fb3ffc2bfb6fce05c0a48a845
Bogdan-Wdev <121345087+Bogdan-Wdev@users.noreply.github.com>
2023-10-29 20:12:08 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new Swiper('.header__slider'),{
+    parallax:true,
+    speed:2000,
+    autoplay:true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    }
+}
+
