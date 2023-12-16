4d90c1efdaacd4d1d610c4955442f3177efd85ef
Bogdan-Wdev <121345087+Bogdan-Wdev@users.noreply.github.com>
2023-12-16 16:27:53 +0200

    Add files via upload

---
+++ lesson_21/js/device.js
@@ -0,0 +1,30 @@
+"use strict"
+
+const isMobile = {
+    Android: function() {
+        return window.matchMedia("only screen and (max-width: 600px)").matches;
+    },
+    iOS: function() {
+        return window.matchMedia("only screen and (max-width: 768px)").matches;
+    },
+    any: function() {
+        return isMobile.Android() || isMobile.iOS();
+    },
+};
+
+if (isMobile.any()){
+    document.body.classList.add('_touch');
+
+    let menuArrows = document.querySelectorAll('.menu__arrow');
+    if(menuArrows.length>0) {
+        for (let index = 0; index < menuArrows.length; index++){
+            const menuArrow = menuArrows[index];
+            menuArrow.addEventListener("click", function(e) {
+                menuArrow.parentElement.classList.toggle('_active');
+            });
+        }
+    }
+    
+} else {
+    document.body.classList.add('_pc');
+}
--- lesson_21/js/main.js
+++ lesson_21/js/main.js
@@ -1,10 +1,26 @@
-new Swiper('.header__slider'),{
-    parallax:true,
-    speed:2000,
-    autoplay:true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    }
-}
+
+new Swiper('.swiper',{
+  loop:true,
+  pagination:{
+    el:'.swiper-pagination',
+    type:'fraction',
+  },
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var input = document.getElementById('search-input');
+  var label = document.querySelector('.form-header__label');
+
+  label.addEventListener('click', function () {
+      input.classList.toggle('_active');
+  });
+
+  document.addEventListener('click', function (event) {
+      // Проверяем, был ли клик вне элемента label
+      if (!label.contains(event.target) && !input.contains(event.target)) {
+          input.classList.remove('_active');
+      }
+  });
+});
 
